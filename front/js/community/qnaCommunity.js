cb7b6f792bb39678947dfdfbe3d17ad95f6e1c3e
LAPTOP-UHOL95SQ\leana <diaperi0901@gmail.com>
2023-07-26 10:05:06 +0900

    커뮤니티/230726

--- front/js/community/qnaCommunity.js
+++ front/js/community/qnaCommunity.js
@@ -1,5 +1,5 @@
 function autoResizeTextarea() {
     const textarea = document.getElementById("qWriteReplyInput");
-    textarea.style.height = "auto"; // 높이를 자동으로 조절하도록 초기화
+    textarea.style.height = "30px"; // 높이를 자동으로 조절하도록 초기화
     textarea.style.height = textarea.scrollHeight + "px"; // 스크롤 높이로 설정
 }
